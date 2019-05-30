7b09fbbfce68a993dc073b4ab2539e104f9fd029
JPZCode <jpzcode02@gmail.com>
2019-05-30 11:31:53 -0500

    Introduciendo API

--- screens/LinksStack.js
+++ screens/LinksStack.js
@@ -1,18 +1,53 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import {
+  ScrollView,
+  StyleSheet,
+  ActivityIndicator
+} from 'react-native';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
   };
 
+  constructor() {
+    super();
+    this.state = {
+      dogs: ''
+    }
+  }
+
+  componentDidMount() {
+    this._fetchDogs();
+  }
+
+  _fetchDogs = () => {
+    let dogPetition = new XMLHttpRequest;
+    dogPetition.open('https://dog.ceo/api/breed/hound/images');
+    dogPetition.onreadystatechange = () => {
+      if (dogPetition.status == 200 && dogPetition.readyState == 4) {
+        this.setState({
+          dogs: JSON.parse(dogPetition.resposeText)
+        })
+      }
+    }
+    dogPetition.send();
+  }
+
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        {
+          this.state.dogs ?
+            this.state.dogs.message.map((dog, key) => {
+              return (
+                <View style={styles.card}>
+                
+                </View>
+              )
+            })
+            : <ActivityIndicator/>
+        }
       </ScrollView>
     );
   }
