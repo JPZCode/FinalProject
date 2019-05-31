2fe3311c8d755232ec156831470e7783b8164f6c
JPZCode <jpzcode02@gmail.com>
2019-05-31 10:13:48 -0500

    Pruebas de buscador

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,14 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-import Register from '../screens/Register';
+//import Register from '../screens/Register';
 import LogNavigator from '../navigation/LogInUpTabNavigator';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  TabNavigator: LogNavigator,
-  Main: Register,
+  
+  //TabNavigator: LogNavigator,
   Navigator: MainTabNavigator,
+
 }));
--- screens/SettingsStack.js
+++ screens/SettingsStack.js
@@ -18,33 +18,48 @@ export default class SettingsScreen extends React.Component {
     super();
     this.state = {
       dogs: '',
-      user: ''
+      breed: '',
     }
   }
 
-  updateUser = (user) => {
-     this.setState({ user: user })
+  componentDidUpdate(){
+    this._picker();
   }
 
-  async _fetch() {
-    let response = await fetch('https://dog.ceo/api/breed/(Raza)/images/random/');
-    const json = await response.json();
-    this.setState({
-      dogs: json
-    });
-    console.log(this.state.dogs);
+  _fill = (breeds) => {
+    let valor = 'https://dog.ceo/api/breed/'+breeds+'/images/random/';
+    return valor;
+  }
+
+  async _picker() {
+    let breeds = this.state.breed;
+    console.log(this._fill(breeds));
+    
+    fetch(this._fill(breeds))
+    .then((valor) => {
+        const parsed = JSON.parse(valor);
+        this.setState({
+          dogs: parsed[0]
+        });
+        console.log(this.state)
+    })
+    .catch(error => console.log(error))
+
+    console.log(this.state)
   }
 
   render() {
     return (
       <View style={styles.container}>
-         <Picker selectedValue={this.state.user} onValueChange={this.updateUser}>
-           <Picker.Item label = "Swiss Mountain" value = "SwissMountain" />
+         <Picker selectedValue={this.state.breed} 
+          onValueChange={breed => this.setState({breed: breed })}
+          mode="dropdown">
+           <Picker.Item label = "African" value = "african" />
            <Picker.Item label = "Ellen" value = "ellen" />
            <Picker.Item label = "Maria" value = "maria" />
           </Picker>
-          <Button title="Buscar" onPress={()=>this._fetch()} />
-          {
+          {/* <Button title="Buscar" onPress={()=>this._picker()} /> */}
+          {/* {
           this.state.dogs ?
             this.state.dogs.message.map((dog, key) => {
               return (
@@ -52,7 +67,7 @@ export default class SettingsScreen extends React.Component {
               )
             })
             : <ActivityIndicator/>
-        }
+          } */}
       </View>
     )
   }
@@ -62,4 +77,8 @@ const styles = new StyleSheet.create({
   container: {
     flex: 1,
   },
+  image:{
+    width: 100,
+    height: 100
+  }
 })
