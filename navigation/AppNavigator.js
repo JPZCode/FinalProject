f71874b53ca227c0de92ce88d9eecde0fd342567
JPZCode <jpzcode02@gmail.com>
2019-06-01 00:51:24 -0500

    Activamos toda la navegación

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,14 +1,11 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-//import Register from '../screens/Register';
 import LogNavigator from '../navigation/LogInUpTabNavigator';
 
 export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   
-  //TabNavigator: LogNavigator,
+  TabNavigator: LogNavigator,
   Navigator: MainTabNavigator,
 
 }));
